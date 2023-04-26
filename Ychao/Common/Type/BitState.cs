f3e353e4a5c2bf9bcfc4e3c5c44e39a5adb234e0
JimryYchao <17889982535@163.com>
2023-04-26 18:48:05 +0800

    sync

---
+++ Ychao/Common/Type/BitState.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Buffers.Binary;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Ychao
+{
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential)]
+    public struct BitState : IEquatable<BitState>, IEnumerable<bool>
+    {
+        //struct BitView
+        //{
+
+        //}
+
+        private volatile uint m_bits;
+        public const int MaxSize = sizeof(uint);
+        public const int MinSize = 1;
+        public const int DefaultSize = 16;
+
+        public readonly static object s_locker = new object();
+        public readonly int size;
+
+        public override string ToString()
+        {
+            return Convert.ToString(m_bits & ((1 << size) - 1), 2).PadLeft(size, '0');
+        }
+
+        public BitState(ushort size)
+        {
+            this.size = size > 0 && size <= MaxSize ? size : DefaultSize;
+            m_bits = 0;
+        }
+
+        public BitState(uint bit, int size)
+        {
+            this.m_bits = bit;
+            this.size = size;
+        }
+
+        public bool this[int i]
+        {
+            get
+            {
+                CheckIndex(i);
+                return ((i << i) & m_bits) > 0;
+            }
+
+            set
+            {
+                CheckIndex(i);
+                if (value)
+                    Open((uint)1 << i);
+                else
+                    Close((uint)1 << i);
+            }
+        }
+
+        uint MaskNormalize(IEnumerable<int> bits)
+        {
+            uint mask = 0;
+            foreach (var item in bits)
+            {
+                CheckIndex(item);
+                mask |= ((uint)1 << item);
+            }
+            Console.WriteLine(Convert.ToString(mask, 2).PadLeft(size, '0'));
+            return mask;
+        }
+        void CheckIndex(int i)
+        {
+            if (i < 0 || i >= size)
+                throw new IndexOutOfRangeException();
+        }
+
+        public void Clear()
+        {
+            m_bits = 0;
+        }
+
+        public void Mask(uint state)
+        {
+            lock (s_locker)
+                m_bits &= state;
+        }
+        public void Mask(IEnumerable<int> bits)
+        {
+            lock (s_locker)
+                m_bits &= MaskNormalize(bits);
+        }
+
+        public bool Check(int index)
+        {
+            return this[index];
+        }
+
+        public void SwitchAll()
+        {
+            m_bits = ~m_bits;
+        }
+
+        public void Inverse(IEnumerable<int> bits)
+        {
+            lock (s_locker)
+            {
+                foreach (var i in bits)
+                {
+                    CheckIndex(i);
+                    this[i] = !this[i];
+                }
+            }
+        }
+
+        public void Open(uint state)
+        {
+            m_bits |= state;
+        }
+
+        public void Open(IEnumerable<int> bits)
+        {
+            lock (s_locker)
+                m_bits |= MaskNormalize(bits);
+        }
+
+        public void Close(uint state)
+        {
+            m_bits &= ~state;
+        }
+
+        public void Close(IEnumerable<int> bits)
+        {
+            lock (s_locker)
+                m_bits &= ~MaskNormalize(bits);
+        }
+
+        public void Switch(uint state)
+        {
+            m_bits ^= state;
+        }
+
+        public void Switch(IEnumerable<int> bits)
+        {
+            m_bits ^= MaskNormalize(bits);
+        }
+
+        public bool Equals(BitState other)
+        {
+            if (size != other.size)
+                return false;
+            var mask = uint.MaxValue << size;
+            return (m_bits | mask) == (other.m_bits | mask);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public IEnumerator<bool> GetEnumerator()
+        {
+            return new BitEnunerator(this);
+        }
+
+
+        public static implicit operator bool[](BitState bit)
+        {
+            bool[] bits = new bool[bit.size];
+            lock (s_locker)
+                for (int i = 0; i < bit.size; i++)
+                    bits[i] = bit[i];
+
+            return bits;
+        }
+
+        struct BitEnunerator : IEnumerator<bool>
+        {
+            private bool current;
+            public bool Current => current;
+
+            BitState bitGroup;
+            int i;
+
+            public BitEnunerator(BitState bit)
+            {
+                this.current = false;
+                this.bitGroup = bit;
+                i = 0;
+            }
+            object IEnumerator.Current => Current;
+
+            void IDisposable.Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                if (i >= bitGroup.size)
+                    return false;
+                current = bitGroup[i];
+                i++;
+                return true;
+            }
+
+            void IEnumerator.Reset()
+            {
+            }
+        }
+    }
+}
--- dotnet-learn/Program.cs
+++ dotnet-learn/Program.cs
@@ -1,14 +1,31 @@
 ﻿using dotnet_learn.SystemDiagnostics.DebugTest;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System;
+using System.Collections;
+using Ychao;
 
 namespace dotnet_learn
-{ 
+{
     internal class Program
     {
+        static BitState bit;
         static void Main(string[] args)
         {
-            //DebuggerTests.Instance.Test();
+            bit = new BitState(0b_1_0101_1101, 9);
+            bit.Mask(new List<int> { 1, 2, 3, 4 });
+            Console.WriteLine(bit);
 
-            DebugProviderTests.Instance.Test(); 
+            bit = new BitState(0b_1_0101_1101, 9);
+            bit.Close(0b10100);
+            Console.WriteLine(bit);
+
+            bit.Open(1 << 4);
+            Console.WriteLine(bit);
+
+            bit.Switch(1 << 3);
+            Console.WriteLine(bit);
+
         }
     }
 }
